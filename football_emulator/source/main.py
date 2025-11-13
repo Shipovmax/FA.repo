b45476ab873f12f32d92fb03b56afa0fc6b6518e
Anatoliy Konovalov <anatoliykonovalov2008@gmail.com>
2025-11-13 13:17:19 +0300

    Сделал джейсон

--- football_emulator/source/main.py
+++ football_emulator/source/main.py
@@ -4,6 +4,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
 
-from football_emulator.templates.ended import print_ended
+from football_emulator.templates.header import *
+from football_emulator.templates.ended import *
 
-print_ended()
+print_header()
